ba01bd8e923d8f255ce0215a09342901c68059bf
joaovictoraraujodev <123034411+joaovictoraraujodev@users.noreply.github.com>
2024-04-04 20:14:27 -0300

    Add files via upload

---
+++ Curso-Js/aula10/index.js
@@ -0,0 +1,4 @@
+/* 1 - pode se redeclarar variáveis quando utilizar var, com let não.
+2 - não crie variáveis sem especificar se é var, let ou const.
+3 - não vamos usar mais var, então só usaremos let ou const.
+*/
